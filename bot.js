b3502a71012f6f2991af903eab6a67ec40802570
hamof <40973927+hamof@users.noreply.github.com>
2018-08-30 22:14:45 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -76,7 +76,7 @@ client.on('message', async msg => { // eslint-disable-line
 			        .setDescription(`**الرجآء من حضرتك إختيآر رقم المقطع** :
 ${videos.map(video2 => `[**${++index} **] \`${video2.title}\``).join('\n')}`)
 //by ,$ ReBeL ء , ??#4777 'CODES SERVER'
-					.setFooter("CODES")
+					.setFooter("Hamo bot")
 					msg.channel.sendEmbed(embed1).then(message =>{message.delete(20000)})
 					
 					// eslint-disable-next-line max-depth
